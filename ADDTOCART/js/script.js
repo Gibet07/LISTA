61a74d2ccd9de855cf1903d40d8878fec02943f6
Gibet07 <gdominguez20060@iesjoanramis.org>
2024-10-24 08:55:23 +0200

    Add files via upload

--- ADDTOCART/js/script.js
+++ ADDTOCART/js/script.js
@@ -1,21 +1,43 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const input = document.getElementById("inputField");
 const boto = document.getElementById("afegir");
 const lista = document.getElementById("llista");
 
 const appSettings = {
-      databaseURL: "https://addtocart-3079a-default-rtdb.europe-west1.firebasedatabase.app"
+      databaseURL: "https://addtocart-3079a-default-rtdb.europe-west1.firebasedatabase.app/"
      }
 const app = initializeApp(appSettings);
 const baseDades = getDatabase(app);
 const tasks = ref(baseDades, "tareas");
 
-
 boto.addEventListener("click", function() {
-      push(tasks, input.value) 
-      lista.innerHTML += `<li>${input.value}</li>`
+      push (tasks, input.value) 
+      Clearscreen();
+})
+
+function addElement(e) {
+
+      let elementLlista = document.createElement("li");
+      elementLlista.id=e[0]
+      elementLlista.textContent=e[1]
+      lista.append(elementLlista)
+     
+}
+
+function Clearscreen(){
       input.value = ""
-      
+}
+function ClearList(){
+      lista.innerHTML = "";
+}
+
+onValue(tasks, function (snapshot) {
+      let resultats = Object.entries(snapshot.val())
+      ClearList();
+      for(let i = 0; i < resultats.length; i++) {
+            let current = resultats[i]
+            addElement(current)
+      }
 })
